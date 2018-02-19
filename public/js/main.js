e3e14f31c6751ee919908a79c906060486d0a445
Danielapariona <daniela.parionac@gmail.com>
2018-02-19 14:41:18 -0500

    Fix show comments

--- public/js/main.js
+++ public/js/main.js
@@ -5,29 +5,35 @@ $('#add-theme').on('click', function () {
 
 $('.reply-comment-js').on('click', function () {
   const parentComments = $(this).parent().parent().parent().attr('id');
-  $("#" + parentComments + " .comments").append(`
-    <form class="ui reply form">
-      <div class="ui form">
-        <div class="field">
-          <label>Por:</label>
-          <input type="text" class="author-comment">
+  const comments = $("#" + parentComments + " .form-comments-js");
+
+  if (comments.hasClass('hidden')) {
+    comments.removeClass('hidden');
+    comments.addClass('content-block');
+    comments.append(`
+      <form class="ui reply form">
+        <div class="ui form">
+          <div class="field">
+            <label>Por:</label>
+            <input type="text" class="author-comment">
+          </div>
         </div>
-      </div>
-      <br>
-      <div class="field">
-        <label>Mensaje</label>
-        <textarea></textarea>
-      </div>
-      <div class="ui blue labeled submit icon button">
-        <i class="icon edit"></i> Agregar comentario
-      </div>
-    </form>`
-  );
-  $("#" + parentComments + " .form-comments-js").toggle();
-  $('.author-comment').focus();
+        <br>
+        <div class="field">
+          <label>Mensaje</label>
+          <textarea></textarea>
+        </div>
+        <div class="ui blue labeled submit icon button">
+          <i class="icon edit"></i> Agregar comentario
+        </div>
+      </form>`
+    );
+    $('.author-comment').focus();
+  } else if (comments.hasClass('content-block')) {
+    comments.removeClass('content-block');
+    comments.addClass('hidden');
+    const form = $("#" + parentComments + " .form-comments-js" + " form");
+    form.remove();
+  }
 });
-
-
-
-
 
