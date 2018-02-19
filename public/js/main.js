1278156ce08e7eeaadf6e512705e75283698fdbb
Danielapariona <daniela.parionac@gmail.com>
2018-02-19 11:56:03 -0500

    Add toggle for comments

--- public/js/main.js
+++ public/js/main.js
@@ -3,3 +3,13 @@ $('#add-theme').on('click', function () {
   .modal('show');
 })
 
+$('.reply-comment-js').on('click', function() {
+
+  const parentComment = $(this).parent().parent().parent().attr('id');
+  // console.log(parentComment);
+  $("#"+ parentComment + " .form-comments-js").toggle();
+  $('.author-comment').focus();
+})
+
+
+
