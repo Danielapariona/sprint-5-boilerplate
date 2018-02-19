426f6f8b332f42801ebf98a741e4a6fb1b26acd8
Danielapariona <daniela.parionac@gmail.com>
2018-02-19 12:25:13 -0500

    Add form dynamic

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,33 @@
 $('#add-theme').on('click', function () {
   $('.ui.small.modal')
-  .modal('show');
+    .modal('show');
 })
 
-$('.reply-comment-js').on('click', function() {
-
-  const parentComment = $(this).parent().parent().parent().attr('id');
-  // console.log(parentComment);
-  $("#"+ parentComment + " .form-comments-js").toggle();
+$('.reply-comment-js').on('click', function () {
+  const parentComments = $(this).parent().parent().parent().attr('id');
+  $("#" + parentComments + " .comments").append(`
+    <form class="ui reply form">
+      <div class="ui form">
+        <div class="field">
+          <label>Por:</label>
+          <input type="text" class="author-comment">
+        </div>
+      </div>
+      <br>
+      <div class="field">
+        <label>Mensaje</label>
+        <textarea></textarea>
+      </div>
+      <div class="ui blue labeled submit icon button">
+        <i class="icon edit"></i> Agregar comentario
+      </div>
+    </form>`
+  );
+  $("#" + parentComments + " .form-comments-js").toggle();
   $('.author-comment').focus();
-})
+});
+
+
 
 
 
